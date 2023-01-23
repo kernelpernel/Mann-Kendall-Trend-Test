997b7aafad76ada601d9524b4a8b6c98e1306b22
kernelpernel <epurnell1996@gmail.com>
2023-01-23 13:39:01 -0500

    Rough version of program completed. Still need to handle a few special cases and clean up a bit

--- MKtest.py
+++ MKtest.py
@@ -6,6 +6,9 @@ Date: 1/23/23
 """
 
 import pandas as pd
+import math
+import scipy
+import statistics
 
 
 def read_data():
@@ -30,11 +33,11 @@ def sign(x):
 
 
 def calculate_test_statistics(timeseries_list):
-    n = len(items)
+    n = len(timeseries_list)
     results = []
 
-    for item in items:
-        i = items.index(item)
+    for item in timeseries_list:
+        i = timeseries_list.index(item)
         j = i + 1
 
         while j < n:
@@ -49,9 +52,88 @@ def calculate_test_statistics(timeseries_list):
     return S, tau
 
 
+def calculate_sigma_s(timeseries_list):
+    # Currently assumes no ties, will build functionality in later if needed
+    n = len(timeseries_list)
+    radicand = (n/18)*(n-1)*(2*n + 5)
+
+    return math.sqrt(radicand)
+
+
+def calculate_Z_statistic(S, sigma_s):
+    if S > 0:
+        return (S - 1) / sigma_s
+
+    elif S < 0:
+        return (S + 1) / sigma_s
+
+    else:
+        return 'tie'
+
+
+def calculate_p_value(z, one_tailed=True):
+    if one_tailed:
+        return scipy.stats.norm.sf(abs(z))
+
+    else:
+        return scipy.stats.norm.sf(abs(z)) * 2
+
+
+def analyze_trend(stats_dict):
+    for key, value in stats_dict.items():
+        S = value[0]
+        tau = round(value[1], 3)
+        CF = round(value[4], 5)
+        CoV = value[6]
+        if S > 0:
+            if CF > 0.95:
+                print(key)
+                print('Trend Result: Increasing')
+                print('Test Statistics: S = ' + str(S) + ', Tau = ' + str(tau) + ', CF = ' + str(CF*100) + '%')
+                print('\n')
+
+            if 0.95 >= CF >= 0.9:
+                print(key)
+                print('Trend Result: Probably Increasing')
+                print('Test Statistics: S = ' + str(S) + ', Tau = ' + str(tau) + ', CF = ' + str(CF*100) + '%')
+                print('\n')
+
+            if CF < 0.9:
+                print(key)
+                print('Trend Result: No Trend')
+                print('Test Statistics: S = ' + str(S) + ', Tau = ' + str(tau) + ', CF = ' + str(CF*100) + '%')
+                print('\n')
+
+        if S <= 0:
+            if CF < 0.9 and CoV >= 1:
+                print(key)
+                print('Trend Result: No Trend')
+                print('Test Statistics: S = ' + str(S) + ', Tau = ' + str(tau) + ', CF = ' + str(CF*100) + '%')
+                print('\n')
+
+            if CF < 0.9 and CoV < 1:
+                print(key)
+                print('Trend Result: Stable')
+                print('Test Statistics: S = ' + str(S) + ', Tau = ' + str(tau) + ', CF = ' + str(CF*100) + '%')
+                print('\n')
+
+        if S < 0:
+            if CF > 0.95:
+                print(key)
+                print('Trend Result: Decreasing')
+                print('Test Statistics: S = ' + str(S) + ', Tau = ' + str(tau) + ', CF = ' + str(CF*100) + '%')
+                print('\n')
+
+            if 0.95 >= CF >= 0.9:
+                print(key)
+                print('Trend Result: Probably Decreasing')
+                print('Test Statistics: S = ' + str(S) + ', Tau = ' + str(tau) + ', CF = ' + str(CF*100) + '%')
+                print('\n')
+
+
 data = read_data()
 
-column_S_values = {}
+column_stats = {}
 
 for column, values in data.items():
     if column == 'Year':
@@ -61,9 +143,14 @@ for column, values in data.items():
         values.dropna(inplace=True)
         items = list(values.values)
         S, tau = calculate_test_statistics(items)
-        entry = {column: [S, tau]}
-        column_S_values.update(entry)
-# print(X)
+        # The z-stat only valid if n > 10. Will build in later if needed. Some data sets have n = 10... will just
+        # assume for now that they meet the requirements for this
+        sigma_s = calculate_sigma_s(items)
+        z = calculate_Z_statistic(S, sigma_s)
+        p = calculate_p_value(z)
+        CF = 1-p
+        CoV = statistics.stdev(items) / statistics.mean(items)
+        entry = {column: [S, tau, sigma_s, z, CF, p, CoV]}
+        column_stats.update(entry)
 
-print(column_S_values)
-
+analyze_trend(column_stats)
